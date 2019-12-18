6969c6731f72c42d9ebfa38faa16f65aa061f87a
mmicciolo <mmicciolo@wpi.edu>
2019-12-17 23:52:47 -0500

    Initial Commit

---
+++ src/main/java/org/wlcp/wlcpgateway/WlcpGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.wlcp.wlcpgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlcpGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlcpGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wlcp/wlcpgateway/WlcpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wlcp.wlcpgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlcpGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
