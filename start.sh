47807b40d9099e27570fe2a4310713a5567f2ade
mmicciolo <mmicciolo@wpi.edu>
2020-01-05 22:37:11 -0500

    Fixed bug in start script with ip address quotes

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ "$SPRING_PROFILES_ACTIVE" = "ecs" ]
 then
-export ECS_INSTANCE_IP_ADDRESS=$(curl http://169.254.170.2/v2/metadata | jq '.Containers[0].Networks[0].IPv4Addresses[0]')
+export ECS_INSTANCE_IP_ADDRESS=$(curl http://169.254.170.2/v2/metadata | jq '.Containers[0].Networks[0].IPv4Addresses[0]' | tr -d '"')
 echo ${ECS_INSTANCE_IP_ADDRESS}
 fi
 
